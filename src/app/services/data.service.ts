c318dea93e955db9c90207c66df2670a2e3d1cbe
Jose Alejandro Jimenez Rosa <ja.jimenezrosa@gmail.com>
2020-01-01 18:31:21 -0400

    Ion-skeleton-text Exmaples and exercices.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Componente } from './../pages/interfaces/interfaces';
 import { Injectable } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-
+import  { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -26,7 +26,10 @@ getAlbumes(){
 
 
 getHeroes(){
-  return this.http.get('/assets/data/superheroes.json');
+  return this.http.get('/assets/data/superheroes.json')
+        .pipe(
+          delay(2000)
+        );
 }
 
 
